92647a12a68cfc394f6868c326c6ba39e9b5bf4e
Jasper Chen <jaazpa3@gmail.com>
2014-12-18 02:57:20 -0500

    adding files and some additional changes

--- errbot/ERRbot.py
+++ errbot/ERRbot.py
@@ -12,15 +12,19 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, PoseArray, Pose, Point, Quaternion, Vector3
-from std_msgs.msg import String, Int64
+from std_msgs.msg import String, Int64, Header
+import tf
+
 
 class ERRbotMain:
 
 	def __init__(self):
+		self.listener = tf.TransformListener()
 		rospy.Subscriber("scan", LaserScan, queue_size=1)
-		self.vel_pub=rospy.Publisher('cmd_vel',Twist,queue_size=1)
+		#self.vel_pub=rospy.Publisher('cmd_vel',Twist,queue_size=1)
 		self.chatter_pub=rospy.Publisher('chatter',String,queue_size=10)
-		#self.vision_pub=rospy.Publisher("vision_pub",Int64,queue_size=10)
+		self.move_simple_base_pub = rospy.Publisher("/move_base_simple/goal", PoseStamped)
+		self.pose_pub = rospy.Publisher("pose",Pose,queue_size=1)#self.vision_pub=rospy.Publisher("vision_pub",Int64,queue_size=10)
 
 		#self.vision = rospy.Subscriber("Vision", Int64, self.vision_callback, queue_size=1)
 		self.map = rospy.Subscriber("Map", Int64, queue_size=1)
@@ -39,6 +43,39 @@ class ERRbotMain:
 		self.vision_flag = False
 		self.wall_follow_flag = False
 
+	def tf_listen(self):
+		try:
+			(pose,quaternion) = self.listener.lookupTransform('/base_link','/map',rospy.Time())
+			self.pose_pub(pose)
+			self.thing()
+		except:
+			pass
+		
+		#print pose, quaternion
+
+
+	def go_to_goal(self):
+		position = Point()
+		orientation = Quaternion()
+
+		position.x = randint(0,9)
+		position.y = randint(0,9)
+		position.z = 0
+		orientation.x = 0
+		orientation.y = 0
+		orientation.w = 1
+		orientation.z = 0
+		send_goal_topic(position,orientation)
+
+	def send_goal_topic(self, pos, orientation_quaternion, frame_id='map'):
+		pos.z = 0
+		header = Header()
+		header.stamp = rospy.Time.now()
+		header.frame_id = frame_id
+		base_goal = PoseStamped(header,Pose(position=pos, orientation=orientation_quaternion))
+		self.running = True
+		self.move_simple_base_pub.publish(base_goal)
+
 	def vision_callback(self,data):
 		self.vision_data = data
 		self.vision_flag = True
@@ -53,6 +90,9 @@ class ERRbotMain:
 		if self.wall_follow_flag == True: #when data arrives
 			pass
 			#self.vel_pub.publish(self.wall_follow_data)
+
+		#pub.publishrostopic pub /move_base_simple/goal geetry_msgs/PoseStamped '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: 1.0, y: 0.0, z: 0.0}, orientation: {w: 1.0}}}'
+
 
 		#something that talks to dead reckoning
 
@@ -81,5 +121,8 @@ if __name__ == '__main__':
 	n = ERRbotMain()
 	while not(rospy.is_shutdown()):
 
+
 		n.arbiter()
-		n.thing()
+		#n.thing()
+		#n.go_to_goal()
+		n.tf_listen()
